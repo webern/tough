13b1be5931bc5099cb17e6395abac5fc46e38884
Matthew James Briggs <matthew.james.briggs@gmail.com>
2020-10-27 13:50:39 -0700

    tough: remove type parameter for transport
    Removes both the lifetime and type parameters for the Repository struct.
    By holding the Transport as a trait object, using Box<dyn Transport>,
    the Repository struct no longer needs a type parameter.
    
    In order to accomplish this, it was necessary to remove associated types
    from the Transport trait. This leads us to return a Box<dyn Read>
    and to introduce a concrete TransportError type to be used by
    implementors of the Transport trait.
    
    The upside of this change is that, for the most common known use cases
    of Repository use (file and http), it is no longer necessary to make the
    code generic on T: Transport. The trade-off is a slight bit of
    additional work for those that would implement their own Transport
    trait.
    
    Some refactoring in the HTTP transport was called for. Though not
    strictly necessary, the retry loop was so difficult to understand that
    I found it necessary to factor out the inspection of the Result we get
    from reqwest into its own set of functions.

--- tough/src/transport.rs
+++ tough/src/transport.rs
@@ -1,36 +1,141 @@
-use std::io::Read;
+use serde::export::Formatter;
+use std::fmt::{Debug, Display};
+use std::io::{ErrorKind, Read};
 use url::Url;
 
 /// A trait to abstract over the method/protocol by which files are obtained.
-pub trait Transport {
-    /// The type of `Read` object that the `fetch` function will return.
-    type Stream: Read;
-
-    /// The type of error that the `fetch` function will return.
-    type Error: std::error::Error + Send + Sync + 'static;
-
+///
+/// The trait hides the underlying types involved by returning the `Read` object as a
+/// `Box<dyn Read>` and by requiring concrete type [`TransportError`] as the error type.
+///
+pub trait Transport: Debug {
     /// Opens a `Read` object for the file specified by `url`.
-    fn fetch(&self, url: Url) -> Result<Self::Stream, Self::Error>;
+    fn fetch(&self, url: Url) -> Result<Box<dyn Read>, TransportError>;
+
+    /// Returns a clone of `self` as a `Box<dyn Transport>`.
+    ///
+    /// # Why
+    ///
+    /// The [`Repository`] object holds a `Box<dyn Transport>`, and because we want the `Repository`
+    /// object to implement `Clone`, we need a way of cloning the boxed `Transport` without knowing
+    /// its underlying type. We cannot require require the `Clone` trait bound on `Transport`
+    /// because, if we do, the trait can no longer serve as a 'trait object'.
+    ///
+    /// # How
+    ///
+    /// If your `Transport` object implements clone, then:
+    ///
+    /// ```rust,ignore
+    /// fn boxed_clone(&self) -> Box<dyn Transport> {
+    ///     Box::new(self.clone())
+    /// }
+    /// ```
+    ///
+    fn boxed_clone(&self) -> Box<dyn Transport>;
 }
 
-/// Provides a `Transport` for local files.
+// =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=
+
+/// The kind of error that the transport object experienced during `fetch`.
+///
+/// # Why
+///
+/// Some TUF operations need to know if a [`Transport`] failure is a result of a file not being
+/// found. In particular:
+/// > 5.1.2. Try downloading version N+1 of the root metadata file `[...]` If this file is not
+/// > available `[...]` then go to step 5.1.9.
+///
+/// To distinguish this case from other [`Transport`] failures, we use
+/// `TransportErrorKind::FileNotFound`.
+///
+#[derive(Debug, Copy, Clone)]
+#[non_exhaustive]
+pub enum TransportErrorKind {
+    /// The trait does not handle the URL scheme named in `String`. e.g. `file://` or `http://`.
+    UnsupportedUrlScheme,
+    /// The file cannot be found.
+    FileNotFound,
+    /// The transport failed for any other reason, e.g. IO error, HTTP broken pipe, etc.
+    Failure,
+}
+
+/// The error type that [`Transport`] `fetch` returns.
+#[derive(Debug)]
+pub struct TransportError {
+    /// The kind of error that occurred.
+    pub kind: TransportErrorKind,
+    /// The URL that the transport was trying to fetch.
+    pub url: String,
+    /// The underlying error that occurred.
+    pub source: Box<dyn std::error::Error + Send + Sync>,
+}
+
+impl TransportError {
+    /// Creates a new [`TransportError`].
+    pub fn new<S, E>(kind: TransportErrorKind, url: S, source_error: E) -> Self
+    where
+        E: Into<Box<dyn std::error::Error + Send + Sync>>,
+        S: AsRef<str>,
+    {
+        Self {
+            kind,
+            url: url.as_ref().to_owned(),
+            source: source_error.into(),
+        }
+    }
+
+    /// Creates a [`TransportError`] for reporting an unhandled URL type.
+    pub fn unsupported_url<S: AsRef<str>>(url: S) -> Self {
+        TransportError::new(
+            TransportErrorKind::UnsupportedUrlScheme,
+            url,
+            "Transport cannot handle the given URL scheme.".to_string(),
+        )
+    }
+}
+
+/// [`TransportError`] implements the standard error interface.
+impl std::error::Error for TransportError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        let x: &(dyn std::error::Error + 'static) = self.source.as_ref();
+        Some(x)
+    }
+}
+
+/// [`TransportError`] implements display, part of the standard error interface.
+impl Display for TransportError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Transport error '{:?}' for '{}', source: {}",
+            self.kind, self.url, self.source
+        )
+    }
+}
+
+// =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=   =^..^=
+
+/// Provides a [`Transport`] for local files.
 #[derive(Debug, Clone, Copy)]
 pub struct FilesystemTransport;
 
 impl Transport for FilesystemTransport {
-    type Stream = std::fs::File;
-    type Error = std::io::Error;
-
-    fn fetch(&self, url: Url) -> Result<Self::Stream, Self::Error> {
-        use std::io::{Error, ErrorKind};
-
-        if url.scheme() == "file" {
-            std::fs::File::open(url.path())
-        } else {
-            Err(Error::new(
-                ErrorKind::InvalidInput,
-                format!("unexpected URL scheme: {}", url.scheme()),
-            ))
+    fn fetch(&self, url: Url) -> Result<Box<dyn Read>, TransportError> {
+        if url.scheme() != "file" {
+            return Err(TransportError::unsupported_url(url));
         }
+
+        let f = std::fs::File::open(url.path()).map_err(|e| {
+            let kind = match e.kind() {
+                ErrorKind::NotFound => TransportErrorKind::FileNotFound,
+                _ => TransportErrorKind::Failure,
+            };
+            TransportError::new(kind, url, e)
+        })?;
+        Ok(Box::new(f))
+    }
+
+    fn boxed_clone(&self) -> Box<dyn Transport> {
+        Box::new(*self)
     }
 }
